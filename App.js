ff6ceb634d3f3f1e30fa48fdec54c8b0ed7c1c01
NIKIEMAJ <ajulesnikiema@gmail.com>
2024-10-28 15:31:49 +0000

    second commit1  pressable

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import {View, Text, Image, ImageBackground , ScrollView} from 'react-native';
+import {View, Button, Image,Text,Pressable } from 'react-native';
 const LogoImg = require("./assets/adaptive-icon.png")
 export default function App(){
   return(
     <View style={{flex: 1,backgroundColor:"plum", padding: 60}}> 
-  <ScrollView  >
-      <Image source={LogoImg} style={{width:300, height:300}} />
-      <Text>
+     <Button title="press" onPress={()=> console.log("bonjour mes amis")} color="black"/> 
+     <Pressable onPress={() => console.log("image pressed")} > 
+     <Image source={LogoImg} style={{width:300, height:300}} />
+     </Pressable>
+     <Pressable onLongPress={() => console.log(" texte longpressed")} >
+     <Text>
       Bien sûr ! Voici un texte *Lorem Ipsum* pour vos besoins de mise en page :
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor ncididuntut
       labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
@@ -14,8 +17,8 @@ export default function App(){
       proident, sunt in culpa qui officia deserunt mollit anim id est laboCe texte factice est utilisé couramment 
       pour tester la présentation et le design des documents sans être distrait par le contenu.
       </Text>
-      <Image source={LogoImg} style={{width:300, height:300}} />
-  </ScrollView>
+      </Pressable>
+ 
   </View>
 );
 }
