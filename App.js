d25aa5fa0df66b94a7ba73e362beb93dd378a20e
NIKIEMAJ <ajulesnikiema@gmail.com>
2024-10-30 03:04:30 +0000

    3rd day commit : Alert

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import {View,StatusBar,ActivityIndicator } from 'react-native';
+import {View,Button,Alert } from 'react-native';
 
 const LogoImg = require("./assets/adaptive-icon.png")
 export default function App(){
   return(
     <View style={{flex: 1,backgroundColor:"plum", padding: 60}}> 
-   {/* <StatusBar  backgroundColor="green" barStyle= "dark-content" /> */}
-   <ActivityIndicator />
-   <ActivityIndicator  size="large"/>
-   <ActivityIndicator size="large" color="red" />
-   <ActivityIndicator size="large" color="red" animating={true} />
+   <Button title="alert "  color="red"onPress={() => Alert.alert("invalid DATA!") }/>
+   <Button title="alert2 "  color="green"onPress={() => Alert.alert("invalid DATA!", "DOB INCORRET ") }/>
+   <Button title="supprimer"  color="red"onPress={() => Alert.alert("ATTENTION","Voulez vous vraiment supprimer ?",[
+    {
+      text:"Annuler",
+      onPress: () => console.log("cancel pressed")
+    },
+    {
+      text: "oui",
+      onPress: () => console.log( "ok pressed"),
 
+    }
+   ]) }/>
 
 
   </View>
 ); 
 }
-//  onRequestClose={() => setIsModalVisible(false)} est une propriété couramment utilisée avec le composant <Modal>.
-//  Elle est déclenchée lorsque l'utilisateur essaie de fermer le modal de manière native, par exemple en appuyant en dehors 
-//  du modal ou en utilisant le bouton de retour sur les appareils Android.
