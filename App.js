4c0dff5f4726803f43fa659e41ecea97c8fc9b9c
NIKIEMAJ <ajulesnikiema@gmail.com>
2024-11-12 18:34:58 +0000

    box-model , boxshadow, style inheritance, fexbox

--- App.js
+++ App.js
@@ -7,26 +7,63 @@ export default function App(){
     <View style={styles.container}> 
     {/* <Greet name="brice" /> */}
     {/* <Text style={styles.title}>Stylesheet API</Text> */}
-    <View>
-      <Text style={[styles.box ,styles.lightbluebg]}>light blue box </Text>
-    </View><View>
-      <Text  style={[styles.box, styles.lightgreenbg]} >light green box </Text>
+    <View style={styles.darkMode}>
+      <Text style={styles.darkModetext} >
+        style inheritance 
+        <Text style={styles.boldText}> in bold text</Text>
+      </Text>
+    </View>
+    <View style={[styles.box, styles.lightbluebg, styles.androidShadow ]}>
+      <Text style= {{borderRadius :5, backgroundColor:"red "}}>light blue box </Text>
+    </View>
+    <View style={[styles.box, styles.lightgreenbg,styles.androidShadow]}>
+      <Text>light green box </Text>
     </View>
   </View>
 ); 
 }
 const styles = StyleSheet.create({
- container : { flex: 1,backgroundColor:"plum", padding: 60},
+ container : { flex: 1,backgroundColor:"white", padding: 60},
+ darkMode:{
+  backgroundColor:"black",
+ },
+ boldText:{
+  fontWeight : "condensed",
+  color:"red",
+ },
+ darkModetext:{
+  color:"white" 
+ },
  box:{
-  padding : 10,
-  width: 100,
-  height :100,
+  width:150,
+  height:150,
+  paddingHorizontal:10,
+  paddingVertical: 20,
+  marginVertical:10,
+  borderWidth: 2,
   margin : 50
  },
  
  lightbluebg :{backgroundColor:"lightblue"},
 
- lightgreenbg: {
-  backgroundColor :"green"}
+ lightgreenbg: { 
+  backgroundColor :"green"},
+
+  boxShadow:{
+    shadowColor:"red",
+    shadowOffset:{
+      width: 16,
+      height: 16,
+    },
+    shadowOpacity:0.6,
+    shadowRadius:4 
+  },
+  androidShadow:{
+    shadowColor:"red",
+    elevation :50,
+    
+    shadowOpacity:190,
+    shadowRadius:4
+  }
   
 });
